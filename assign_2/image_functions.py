07cb960f6f7d43f7dc8ca4b365214b5979c34949
Sumit Patidar <sumitpatidar96@gmail.com>
2019-03-23 12:50:01 +0530

    Basic structure for Fundamental matrix added

--- assign_2/image_functions.py
+++ assign_2/image_functions.py
@@ -40,3 +40,33 @@ def matrixFormation(pts1, pts2):
         counter = counter + 1
 
     return A, B
+
+# gives the fundamental matrix
+def getFundamental(pts1, pts2):
+
+    #define A matrix
+    A = np.zeros((8, 9))
+    for i in range(9):
+        A.itemset((i, 0), pts1[i][0] * pts2[i][0])
+        A.itemset((i, 1), pts1[i][1] * pts2[i][0])
+        A.itemset((i, 2), pts2[i][0])
+        A.itemset((i, 3), pts1[i][0] * pts2[i][1])
+        A.itemset((i, 4), pts1[i][1] * pts2[i][1])
+        A.itemset((i, 5), pts2[i][1])
+        A.itemset((i, 6), pts1[i][0])
+        A.itemset((i, 7), pts1[i][1])
+        A.itemset((i, 8), 1)
+
+    #get F (fundamental matrix using SVD)
+    u, s, vh = np.linalg.svd(A, full_matrices=True)
+
+    #F is the last column of v
+
+    #decompose F using SVD
+    u_f, s_f, vh_f = u, s, vh = np.linalg.svd(F, full_matrices=True)
+
+    #make the last diagonal element of s_f to be zero
+
+    #multiply u_f, modified s_f, vh_f together to get the low rank F
+
+    return F
